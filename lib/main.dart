6d3ef92b83e85584d5caf51b560f6a7095b5159c
joseph-charbel <josephcharbel3@gmail.com>
2025-10-13 12:14:45 +1100

    Revert "BROKEN: begun implementing forgot password, link does not work"
    This reverts commit 3f4077294c46626ec2c05740b93df11451ab5bc3.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:omada/core/supabase/supabase_instance.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'test_db/epics/epic_home_entry.dart';
 import 'package:omada/core/theme/app_theme.dart';
 import 'package:omada/core/theme/app_theme_controller.dart';
@@ -10,9 +8,6 @@ import 'package:omada/ui/pages/profile_management_page.dart';
 import 'package:omada/ui/pages/splash_page.dart';
 import 'package:omada/ui/pages/login_page.dart';
 import 'package:omada/ui/pages/account_page.dart';
-import 'package:omada/ui/pages/reset_password_page.dart';
-
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,40 +17,16 @@ Future<void> main() async {
   runApp(OmadaRootApp(themeController: themeController));
 }
 
-class OmadaRootApp extends StatefulWidget {
+class OmadaRootApp extends StatelessWidget {
   final AppThemeController themeController;
   const OmadaRootApp({super.key, required this.themeController});
 
   @override
-  State<OmadaRootApp> createState() => _OmadaRootAppState();
-}
-
-class _OmadaRootAppState extends State<OmadaRootApp> {
-  StreamSubscription<AuthState>? _authSub;
-
-  @override
-  void initState() {
-    super.initState();
-    _authSub = supabase.auth.onAuthStateChange.listen((data) {
-      if (data.event == AuthChangeEvent.passwordRecovery) {
-        navigatorKey.currentState?.pushNamed('/reset-password');
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _authSub?.cancel();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<ThemeMode>(
-      valueListenable: widget.themeController.themeMode,
+      valueListenable: themeController.themeMode,
       builder: (context, mode, _) {
         return MaterialApp(
-          navigatorKey: navigatorKey,
           title: 'Omada',
           theme: OmadaTheme.light(),
           darkTheme: OmadaTheme.dark(),
@@ -63,7 +34,6 @@ class _OmadaRootAppState extends State<OmadaRootApp> {
           routes: {
             '/': (_) => const SplashPage(),
             '/login': (_) => const LoginPage(),
-            '/reset-password': (_) => const ResetPasswordPage(),
             '/app': (_) => const ContactsScreen(),
             '/account': (_) => const AccountPage(),
             '/profile': (_) => const ProfileManagementPage(),
