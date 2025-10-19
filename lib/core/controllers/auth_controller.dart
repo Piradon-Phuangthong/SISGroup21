7f4c6d10769b8b8c51be67767c430b52b07d91d6
joseph-charbel <josephcharbel3@gmail.com>
2025-10-19 17:38:03 +1100

    .

--- lib/core/controllers/auth_controller.dart
+++ lib/core/controllers/auth_controller.dart
@@ -27,4 +27,8 @@ class AuthController {
   }
 
   Future<void> signOut() => client.auth.signOut();
+
+  Future<void> resetPasswordForEmail(String email) {
+    return client.auth.resetPasswordForEmail(email);
+  }
 }
